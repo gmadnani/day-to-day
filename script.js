117294c0c8b484f6ac6b5ab577cd4ecf7ff70d08
Girish Madnani <girishmadnani05@gmail.com>
2022-01-22 20:56:30 +0000

    block colors

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ $("#currentDay").html(moment().format('dddd, MMMM Do'))
 
 function BlockColor(){
     
-    var currentHour = moment().hours();
+    var currentHour = moment().hours() - 8;
     console.log(currentHour)
 
     $(".time-block").each(function(){
         var blockHour = parseInt($(this).attr("id"));
         console.log(blockHour)
+
+        if (currentHour > blockHour){
+            $(this).addClass("past")
+        }
+        else if ( currentHour == blockHour){
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("future")
+        } 
     })
 }
+
+BlockColor();
