5b4135aa05a49e9d030739a53d4e4910c8106c70
Girish Madnani <girishmadnani05@gmail.com>
2022-01-23 17:32:29 +0000

    clean code

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
+//adding the current day and date to the html
 $("#currentDay").html(moment().format('dddd, MMMM Do'))
 
-
+//fetching the current hour
 var currentHour = moment().hours();
 
+//function for adding the color blocks to the html
 $(".time-block").each(function(){
+    //getting the block ids
     var blockHour = parseInt($(this).attr("id"));
 
+    //checking the exact hour and comparing it to the block id hour for determining if its past, present or future
     if (currentHour > blockHour){
         $(this).addClass("past")
     }
@@ -18,17 +22,22 @@ $(".time-block").each(function(){
     } 
 })
 
-
+//function for the save button for the local storage of the description text
 $(".saveBtn").on("click", function(){
     
+    //getting the block id of the save button and the description text written in that block id
     var time = $(this).parent().attr("id");
     var textevent = $(this).siblings(".description").val();
 
+    //saving in local storage for that particular block id
     localStorage.setItem(time, textevent)
 })
 
 
+//function for fetching data from the local storage
 $(".hour").each(function(){
+    //getting the block id
     time= $(this).parent().attr("id");
+    //adding the decription text for each block id
     $(this).siblings(".description").val(localStorage.getItem(time));
 })
